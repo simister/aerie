0a7b6e70d2b84d61180fdeb38ab4b2c7958a395f
Simcoe Alkadhi <simcoea@gmail.com>
2024-01-19 16:19:22 -0500

    Update Users Service findAll and findOne methods to return all users and return the details of a single user if they are authenticated.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Request } from 'express';
+
+export type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return await this.prisma.user.findMany({
+      select: { id: true, email: true },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number, req: Request) {
+    const user = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    const decodedUser = req.user as { id: number; email: string };
+
+    if (user.id !== decodedUser.id) {
+      throw new ForbiddenException();
+    }
+
+    delete user.hashedPassword;
+
+    return { user };
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
