b66741a94245c7597e95b3b97456d26c6b9db886
Simcoe Alkadhi <simcoea@gmail.com>
2024-01-19 16:20:44 -0500

    Import JwtStrategy into Users Module and export Users Service.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, JwtStrategy],
+  exports: [UsersService],
 })
 export class UsersModule {}
