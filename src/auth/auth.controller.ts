0a30fc65e43ac1e41a0ff9f2d9dab046240896ef
Simcoe Alkadhi <simcoea@gmail.com>
2024-01-19 14:28:22 -0500

    Add routing for login, logout, and sign-up functionality. Build out sign-up functionality.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,15 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @Post('logout')
+  logOut() {
+    return this.authService.logOut();
+  }
+
+  @Post('signup')
+  signUp(@Body() signUpDto: AuthDto) {
+    return this.authService.signUp(signUpDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,63 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private prisma: PrismaService,
+  ) {}
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(username);
+
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
     const { password, ...result } = user;
     // TODO: Generate a JWT and return it here instead of the user object
     return result;
+    };
+  }
+
+  async logOut() {
+    return { message: 'Logged out.' };
+  }
+
+  async signUp(dto: AuthDto) {
+    const { email, password } = dto;
+
+    const foundUser = await this.prisma.user.findUnique({ where: { email } });
+
+    if (foundUser) {
+      throw new BadRequestException('Email already exists.');
+    }
+
+    const hashedPassword = await this.hashPassword(password);
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        hashedPassword,
+      },
+    });
+
+    return { message: 'Sign-up was successful.' };
+  }
+
+  async hashPassword(password: string) {
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltOrRounds);
+
+    return hashedPassword;
   }
 }
